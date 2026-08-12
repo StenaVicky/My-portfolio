4588ceda3a4dcb1b69f3c3c5f1b67c4b989c6179
Omali stena vicky <omalstena55@gmail.com>
2026-08-12 12:50:34 +0300

    functionality and templates

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import ContactMessage
 
-# Create your views here.
+
+def home(request):
+
+    if request.method == "POST":
+
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        subject = request.POST.get("subject")
+        message = request.POST.get("message")
+
+        ContactMessage.objects.create(
+            name=name,
+            email=email,
+            subject=subject,
+            message=message
+        )
+
+        return redirect("home")
+
+    return render(request, "home.html")
